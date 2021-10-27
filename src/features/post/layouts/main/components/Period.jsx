0ca634a0204403b4e9860818eb6d8483a8cc6475
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-27 17:06:57 +0900

    resources 分岐 削除

--- src/features/post/layouts/main/components/Period.jsx
+++ src/features/post/layouts/main/components/Period.jsx
@@ -1,11 +1,9 @@
 import styles from "../Main.module.scss";
 
-export const Period = ({ period, matters, resources }) => {
+export const Period = ({ period }) => {
   return (
     <div className={styles.main_col}>
-      <span className={styles.main_tag}>
-        {matters ? "開始時期" : resources && "稼働開始時期"}
-      </span>
+      <span className={styles.main_tag}>開始時期</span>
       <p>
         {period?.year}年&nbsp;{period?.month}月予定
       </p>
--- src/features/post/layouts/main/components/header/components/Title.jsx
+++ src/features/post/layouts/main/components/header/components/Title.jsx
@@ -8,12 +8,6 @@ export const Title = ({ post }) => {
       {post.title}&nbsp;
       {newPost && <span className={styles.header_ttl_new}>NEW</span>}
     </h1>
-  ) : post?.roman ? (
-    <h1 className={styles.header_ttl}>
-      {post?.roman?.firstName?.substring(0, 1)}&nbsp;.&nbsp;
-      {post?.roman?.lastName?.substring(0, 1)}&nbsp;
-      {newPost && <span className={styles.header_ttl_new}>NEW</span>}
-    </h1>
   ) : (
     <></>
   );
