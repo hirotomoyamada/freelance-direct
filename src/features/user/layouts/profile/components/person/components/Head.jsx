597798246efbcfa7acb4f80c2eecaca46282af1f
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-28 14:13:05 +0900

    レイアウト 修正

--- src/features/user/layouts/profile/components/person/components/Head.jsx
+++ src/features/user/layouts/profile/components/person/components/Head.jsx
@@ -9,23 +9,28 @@ export const Head = ({ user }) => {
   ][Math.floor(Math.random() * 3)];
 
   return (
-    <div className={styles.profile_wrap}>
+    <div className={styles.profile_col}>
       <h1 className={styles.profile_name}>
         {user?.profile?.nickName ? user.profile.nickName : nickName}
       </h1>
 
-      <div className={styles.profile_category}>{user?.profile?.position}</div>
-      <div
-        className={`${styles.profile_category} ${
-          styles.profile_category_acnt
-        } ${
-          (user?.profile?.state === "確定" ||
-            user?.profile?.state === "商談中" ||
-            user?.profile?.state === "情報収集中") &&
-          styles.profile_category_disable
-        } ${user?.profile?.state === "至急" && styles.profile_category_hurry}`}
-      >
-        {user?.profile?.state}
+      <div className={styles.profile_wrap}>
+        <div className={styles.profile_category}>{user?.profile?.position}</div>
+
+        <div
+          className={`${styles.profile_category} ${
+            styles.profile_category_acnt
+          } ${
+            (user?.profile?.state === "確定" ||
+              user?.profile?.state === "商談中" ||
+              user?.profile?.state === "情報収集中") &&
+            styles.profile_category_disable
+          } ${
+            user?.profile?.state === "至急" && styles.profile_category_hurry
+          }`}
+        >
+          {user?.profile?.state}
+        </div>
       </div>
     </div>
   );
