bd63724da06c1f65b474265d2d2b6ee5f303e0f2
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-04 21:55:08 +0900

    fetch を submit されたときに 変更

--- src/promotion/layouts/section/search/components/form/Form.jsx
+++ src/promotion/layouts/section/search/components/form/Form.jsx
@@ -3,35 +3,19 @@ import styles from "./Form.module.scss";
 import SearchIcon from "@material-ui/icons/Search";
 import CloseIcon from "@material-ui/icons/Close";
 
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useForm } from "react-hook-form";
-
-import { promotionPosts } from "../../../../../../features/post/actions/promotionPosts";
-import * as rootSlice from "../../../../../../features/root/rootSlice";
 import { Btn } from "../../../../../components/btn/Btn";
+import { useSearch } from "../../hook/useSearch";
 
 export const Form = () => {
-  const dispatch = useDispatch();
-  const { register, watch, handleSubmit, reset } = useForm();
-
-  const status = useSelector(rootSlice.verified).status;
-  const value = watch("value");
-
-  useEffect(() => {
-    status === "promo" &&
-      typeof value === "string" &&
-      dispatch(promotionPosts(value));
-  }, [dispatch, status, value]);
-
-  const handleSearch = (data) => {};
-
-  const handleReset = () => {
-    reset({ value: "" });
-  };
+  const [register, handleSubmit, handleSearch, handleReset] = useSearch();
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(handleSearch)}>
+    <form
+      className={styles.form}
+      onSubmit={handleSubmit(handleSearch)}
+      action="#"
+      submit="search"
+    >
       <div className={styles.form_input}>
         <button type="submit">
           <SearchIcon
