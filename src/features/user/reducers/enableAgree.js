5c77e7989c6c74f4e51fab5b58ae1b40224a147b
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-01-26 11:52:47 +0900

    利用規約同意後ページリロード

--- src/features/user/reducers/enableAgree.js
+++ src/features/user/reducers/enableAgree.js
@@ -2,7 +2,11 @@ import { functions } from "../../../firebase";
 
 export const enableAgree = (state) => {
   state.agree = "enable";
-  
+
   const enableAgree = functions.httpsCallable("fd-enableAgree");
-  enableAgree().catch((e) => {});
+  enableAgree()
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((e) => {});
 };
